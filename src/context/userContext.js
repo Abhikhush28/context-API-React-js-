2c563ca558975d5430e775dbdaab59d2638fc756
Abhikhush28 <sahuabhikhush@gmail.com>
2024-01-27 10:07:39 +0530

    context Api discussion

---
+++ src/context/userContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+
+// Har ek context ek major cheez provide karta hai jo ki hai "Provider" end of the ham log wrapper hi use krenge userContext se
+
+export default UserContext;
